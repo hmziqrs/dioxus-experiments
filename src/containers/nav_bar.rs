95a1174debca05e47b5f9b264955a39c0ffa73f2
hmziqrs <hmziqrs@gmail.com>
2025-04-03 18:05:03 +0500

    Add new styles and navigation bar functionality
    - Added new CSS variables and utility classes
    - Implemented navigation bar with links and styles in `nav_bar.rs`

--- src/containers/nav_bar.rs
+++ src/containers/nav_bar.rs
@@ -1,11 +1,42 @@
-use dioxus::prelude::*;
+use dioxus::{html::u::class, prelude::*};
+
+use crate::router::Route;
 
 #[component]
 pub fn ContainerNavBar() -> Element {
+    let mut nav = use_navigator();
+
     rsx! {
         nav {
-            h1 {
-                "Dioxus Experiments"
+            class: "navbar bg-base-200",
+            div {
+                class: "container flex mx-auto",
+                div {
+                    class: "flex-1",
+                    Link {
+                        to: Route::HomeScreen {  },
+                        class: "text-2xl font-medium",
+                        "Dioxus Experiments"
+                    }
+                },
+                menu {
+                    class: "flex-none ",
+                    ul {
+                        class: "menu menu-horizontal space-x-4 cursor-pointer [&>li]:hover:underline",
+                        li {
+                            onclick: move |_| { nav.push(Route::HomeScreen {  }); },
+                            "Home"
+                        },
+                        li {
+                            onclick: move |_| { nav.push(Route::AboutScreen {  }); },
+                            "About"
+                        },
+                        li {
+                            onclick: move |_| { nav.push(Route::LoginScreen {  }); },
+                            "Login"
+                        },
+                    }
+                }
             }
         }
         Outlet::<crate::router::Route> {}
